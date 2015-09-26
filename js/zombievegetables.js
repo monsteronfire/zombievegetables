cbd23121114e5b97b816cb412da00ac2e102bf4f
monsteronfire <monsteronfire@gmail.com>
2015-09-26 20:05:59 +0800

    adds Zombie class

--- js/zombievegetables.js
+++ js/zombievegetables.js
@@ -1,11 +1,19 @@
 window.addEventListener("load", function() {
   var Q = window.Q = Quintus({development: true})
-    .include("Sprites, Scenes")
+    .include("Sprites, Scenes, 2D")
     .setup({
       width: 1080,
       height: 720,
       scaleToFit: true
     });
+
+  Q.Sprite.extend("Zombie", {
+    init: function(p) {
+      this._super(p, {
+        asset: "zombie1.png"
+      })
+    }
+  });
 
   Q.scene("level", function(stage) {
     var sprite1 = new Q.Sprite({
@@ -14,7 +22,25 @@ window.addEventListener("load", function() {
       asset: "background.png"
     });
 
+    var sprite2 = new Q.Zombie({
+      x: 500,
+      y: 600,
+    });
+
+    var sprite3 = new Q.Zombie({
+      x: 700,
+      y: 100,
+    });
+
+    var sprite4 = new Q.Zombie({
+      x: 700,
+      y: 600,
+    });
+
     stage.insert(sprite1);
+    stage.insert(sprite2);
+    stage.insert(sprite3);
+    stage.insert(sprite4);
   });
 
   Q.load("background.png, sun.png, zombie1.png", function() {
