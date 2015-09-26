48b8b3bd266c5ffa9c59a9494624ce066ddab380
monsteronfire <monsteronfire@gmail.com>
2015-09-26 20:25:43 +0800

    adds game loop and turns off gravity

--- js/zombievegetables.js
+++ js/zombievegetables.js
@@ -7,11 +7,32 @@ window.addEventListener("load", function() {
       scaleToFit: true
     });
 
+  Q.gravityY = 0;
+  Q.gravityX = 0;
+
+  Q.SPRITE_SUN = 2;
+  Q.SPRITE_ZOMBIE = 4;
+  Q.SPRITE_PLANT = 8;
+  Q.SPRITE_BULLET = 16;
+  Q.SPRITE_GROUND = 32;
+
   Q.Sprite.extend("Zombie", {
     init: function(p) {
       this._super(p, {
-        asset: "zombie1.png"
-      })
+        asset: "zombie1.png",
+        type: Q.SPRITE_ZOMBIE,
+        collisionMask: Q.SPRITE_PLANT || Q.SPRITE_BULLET,
+        vx: -40
+      });
+      this.add("2d");
+    },
+
+    step: function(dt) {
+      if(this.p.x <= 240) {
+        this.destroy();
+        console.log("The ZUMBEEZ 8 URR BRAINZ!");
+        Q.stageScene("level");
+      }
     }
   });
 
@@ -19,7 +40,8 @@ window.addEventListener("load", function() {
     var sprite1 = new Q.Sprite({
       x: 1080/2,
       y: 720/2,
-      asset: "background.png"
+      asset: "background.png",
+      type: Q.SPRITE_GROUND
     });
 
     var sprite2 = new Q.Zombie({
